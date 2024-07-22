2b7edac0ad5397dee440647d9e95a3cc89fdfdc3
Parash Mainali <mainaliparash@gmail.com>
2024-07-22 16:02:07 +0545

    only frontend

--- backendproj/src/main/java/com/example/backendproj/repository/RoomReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendproj.repository;
-
-
-import com.example.backendproj.model.RoomReservationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomReservationRepository extends JpaRepository<RoomReservationEntity, Long> {
-    boolean existsByRoomNumberAndStatus(int roomNumber, String status);
-}
