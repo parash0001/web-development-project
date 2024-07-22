284743f40a5e6d51981101a91294f20ace148270
Parash Mainali <mainaliparash@gmail.com>
2024-07-22 13:47:53 +0545

    room entity

---
+++ src/main/java/com/example/backendproj/model/RoomEntity.java
@@ -0,0 +1,53 @@
+package com.example.backendproj.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "rooms")
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoomEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private int roomNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoomType type;
+
+    @Column(nullable = false)
+    private double price;
+
+    private boolean isAvailable = true;
+
+    @ElementCollection
+    @CollectionTable(name = "room_images", joinColumns = @JoinColumn(name = "room_id"))
+    @Column(name = "image_url")
+    private List<String> imageUrls;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date createdAt = new java.util.Date();
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date updatedAt = new java.util.Date();
+
+
+
+
+}
